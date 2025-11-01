5dc4f8098397016b211837b66722b1b920610040
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-11-01 13:39:12 +0100

    add: type User and use in const user and tab

--- dist/script.js
+++ dist/script.js
@@ -17,3 +17,26 @@ userRole = UserRole.User;
 userRole = UserRole.Moderator;
 let userIsLogged = true;
 userIsLogged = false;
+const userOne = {
+    login: 'user1',
+    email: 'user1@gmail.com',
+    age: 20,
+    role: UserRole.Admin,
+    isLogged: true,
+};
+const userTwo = {
+    login: 'user2',
+    email: 'user2@gmail.com',
+    age: 30,
+    role: UserRole.Moderator,
+};
+const userThree = {
+    login: 'user3',
+    email: 'user3@gmail.com',
+    age: 40,
+    role: UserRole.Admin,
+};
+const users = [];
+users.push(userOne, userTwo, userThree);
+const numbers = [5, 8, '8'];
+const arr = ['Kacpi', 'admin', 30];
--- src/script.ts
+++ src/script.ts
@@ -21,3 +21,40 @@ userRole = UserRole.Moderator;
 
 let userIsLogged: boolean = true;
 userIsLogged = false;
+
+type User = {
+  login: string;
+  email: string;
+  age: number;
+  role: UserRole;
+  isLogged?: boolean;
+};
+
+const userOne: User = {
+  login: 'user1',
+  email: 'user1@gmail.com',
+  age: 20,
+  role: UserRole.Admin,
+  isLogged: true,
+};
+
+const userTwo = {
+  login: 'user2',
+  email: 'user2@gmail.com',
+  age: 30,
+  role: UserRole.Moderator,
+};
+
+const userThree = {
+  login: 'user3',
+  email: 'user3@gmail.com',
+  age: 40,
+  role: UserRole.Admin,
+};
+
+const users: User[] = [];
+users.push(userOne, userTwo, userThree);
+
+const numbers: [number, number, string] = [5, 8, '8'];
+
+const arr: [string, string, number] = ['Kacpi', 'admin', 30];
